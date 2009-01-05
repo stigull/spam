919c9c43d033eedeccd5fa14a978a7f3867292a7
johannth <johannth@johannth-laptop.(none)>
2009-01-05 16:39:54 +0000

    Removed extra / in urls.py

--- urls.py
+++ urls.py
@@ -5,7 +5,7 @@ from spam.views import decode_email, show_email
 
 urlpatterns = patterns('',
     url(r'^tu-ert-mennskur/', show_email, name='spam-mailto-safe'),
-    url(r'^/$', decode_email),
-    url(r'^/(?P<encoded_email>[A-Za-z0-9._%-]+\+[A-Za-z0-9._%-]+\+[A-Za-z]{2,4})/$', decode_email,
+    url(r'^$', decode_email),
+    url(r'^(?P<encoded_email>[A-Za-z0-9._%-]+\+[A-Za-z0-9._%-]+\+[A-Za-z]{2,4})/$', decode_email,
                     name='spam-decode-email'),
 )
